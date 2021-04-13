326c5ef6829bf68848c93223a376e330312ec1f8
Mattia <Mattia@LAPTOP-3K6IEHAJ>
2021-04-13 10:09:11 +0200

    modifiche finali

--- src/main/java/it/polito/tdp/corsi/FXMLController.java
+++ src/main/java/it/polito/tdp/corsi/FXMLController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 
 import it.polito.tdp.corsi.model.Corso;
 import it.polito.tdp.corsi.model.Model;
+import it.polito.tdp.corsi.model.Studente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -50,6 +51,7 @@ public class FXMLController {
 
     @FXML
     void corsiPerPeriodo(ActionEvent event) {
+    	txtRisultato.clear();
     	
     	String periodoStringa = txtPeriodo.getText();
     	Integer periodo;
@@ -73,14 +75,31 @@ public class FXMLController {
     	}
     	
     	List<Corso> corsi = this.model.getCorsiByPeriodo(periodo);
+    	
+//    	for(Corso c : corsi) {
+//    		txtRisultato.appendText(c.toString()+"\n");
+//    	}
+    	
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+    	
     	for(Corso c : corsi) {
-    		txtRisultato.appendText(c.toString()+"\n");
-    	}
+    		sb.append(String.format("%-8s ", c.getCodins()));
+    		sb.append(String.format("%-4d ", c.getCrediti()));
+    		sb.append(String.format("%-50s ", c.getNome()));
+    		sb.append(String.format("%-4d\n", c.getPd()));
+    		}
+    		
+    		
+    		//- allineaento a sx, 8 dim max della colonna (corso ha max 8 caratteri), s String, d sta per Intero, % guarda che qui sostituiamo questo con il nome che viene dopo
+    		//è tipo il punto interrogativo in DAO, ci vogliono gli spazi tra i get
+    		txtRisultato.appendText(sb.toString());
     }
 
     @FXML
     void numeroStudenti(ActionEvent event) {
-    	
+    	txtRisultato.clear();
     	String periodoStringa = txtPeriodo.getText();
     	Integer periodo;
     	
@@ -103,22 +122,61 @@ public class FXMLController {
     	}
     	
     	Map<Corso, Integer> corsiIscrizioni = this.model.getIscrittiByPeriodo(periodo);
+//    	for(Corso c : corsiIscrizioni.keySet()) {
+//    		txtRisultato.appendText(c.toString());
+//    		Integer n = corsiIscrizioni.get(c);
+//    		txtRisultato.appendText("\t" + n +"\n");
+//    	}
+    	
+StringBuilder sb = new StringBuilder();
+    	
+    	
     	for(Corso c : corsiIscrizioni.keySet()) {
-    		txtRisultato.appendText(c.toString());
-    		Integer n = corsiIscrizioni.get(c);
-    		txtRisultato.appendText("\t" + n +"\n");
+    		sb.append(String.format("%-50s %4d\n", c.getNome(), corsiIscrizioni.get(c)));
     	}
+    	txtRisultato.appendText(sb.toString());	
     	
     }
 
     @FXML
     void stampaDivisione(ActionEvent event) {
-
+    	
+    	txtRisultato.clear();
+    	
+    	String codice = txtCorso.getText();
+    	
+    	if(!model.esisteCorso(codice)) {
+    		txtRisultato.appendText("Il corso non esiste");
+    		return;
+    	}
+    		Map<String, Integer> divisione = model.getDivisioneCDS(codice);
+    		for(String cds : divisione.keySet())
+    			txtRisultato.appendText(cds + " " + divisione.get(cds) + "\n");
+    	
     }
 
     @FXML
     void stampaStudenti(ActionEvent event) {
 
+    	txtRisultato.clear();
+    	
+    	String codice = txtCorso.getText();
+    	
+    	if(!model.esisteCorso(codice)) {
+    		txtRisultato.appendText("Il corso non esiste");
+    		return;
+    	}
+    	List<Studente> studenti = model.getStudentiByCorso(codice);
+    	
+    	if(studenti.size()==0) {
+    		txtRisultato.appendText("Il corso non ha iscritti");
+    		return;
+    	}
+    	
+    	for(Studente s : studenti) {
+    		txtRisultato.appendText(s + "\n");
+    	}
+    	
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -135,6 +193,7 @@ public class FXMLController {
     
     public void setModel(Model model) {
     	this.model = model;
+    	txtRisultato.setStyle("-fx-font-family: monospace");
     }
     
     
--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,5 +23,33 @@ public class Model {
 	public Map<Corso, Integer> getIscrittiByPeriodo(Integer pd) {
 	
 		return corsoDAO.getIscrittiByPeriodo(pd);
+	}
+	
+	public List<Studente> getStudentiByCorso(String codice){
+		return corsoDAO.getStudentiByCorso(new Corso(codice, null, null, null));
+	}
+
+	public boolean esisteCorso(String codice) {
+		
+		return corsoDAO.esisteCorso(new Corso(codice, null, null, null));
+	}
+	
+	public Map<String, Integer> getDivisioneCDS (String codice) {
+		//dato il corso con codice ABC mi aspetto GES->50 INF->40 MEC->30
+		Map<String, Integer> divisione = new HashMap<String, Integer>();
+		List<Studente> studenti = this.getStudentiByCorso(codice);
+		
+//SOLUZIONE1:
+//		for(Studente s : studenti) {
+//			if(s.getCds()!=null &&!s.getCds().equals(""))
+//				if(divisione.get(s.getCds()) == null) {
+//					divisione.put(s.getCds(), 1);
+//				}
+//				else {
+//					divisione.put(s.getCds(), divisione.get(s.getCds()+1));
+//				}
+//		}
+//		
+		return corsoDAO.getDivisioneStudenti(new Corso(codice, null, null, null));
 	}
 }
